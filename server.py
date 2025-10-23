af679cc33f81258bf393f2d7b5fac08ebe007e3a
tommaso-gallo <tommasogallo2016@gmail.com>
2025-10-23 01:03:19 +0100

    added server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "0.0.0.0"
+PORT = 5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Listening on {HOST}:{PORT}...")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        conn.sendall(b"Hello from Raspberry Pi!\n")
